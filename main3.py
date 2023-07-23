fdfe5bd7c733465bc6c090225a6b272b41df08b3
rohitrohilla99 <101062985+rohitrohilla99@users.noreply.github.com>
2023-07-23 22:05:50 +0530

    Add files via upload

--- main3.py
+++ main3.py
@@ -1,25 +1,16 @@
 import turtle
-#star = turtle.Turtle()
- 
-#star.right(75)
-#star.forward(100)
- 
-#for i in range(4):
-    #star.right(144)
-    #star.forward(100)
      
-#turtle.done()
 polygon=turtle.Screen()
 polygon.bgcolor("white")
 polygon=turtle.Turtle()
 #polygon.speed(0)
-num_sides=6
-side_length=200
-angle=360.0/num_sides
+a=6
+b=200
+angle=360.0/a
 polygon.fillcolor("red")
 polygon.begin_fill()
-for i in range(num_sides):
-     polygon.forward(side_length)
+for i in range(a):
+     polygon.forward(b)
      polygon.right(angle)
      polygon.hideturtle()
 polygon.end_fill()  
